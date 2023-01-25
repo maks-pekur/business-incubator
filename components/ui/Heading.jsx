b699b091197fba4ef4e30941f308d6cc9baaa450
Maks Pekur <maxpekur88@gmail.com>
2023-01-25 15:59:06 +0100

    fixed css

--- components/ui/Heading.jsx
+++ components/ui/Heading.jsx
@@ -1,5 +1,5 @@
 const styles = {
-  h1: "lg:text-[40px] tracking-wide	leading-tight	font-bold uppercase my-8 text-center",
+  h1: "text-[26px] lg:text-[40px] tracking-wide	leading-tight	font-bold uppercase my-8 text-center",
   h2: "lg:text-[36px] tracking-wide	leading-tight	font-bold uppercase my-8",
   h3: "md:text-[30px] text-center my-8",
 };

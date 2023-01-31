2ba33c937eca5957c9f4da63785981d778379d76
Maks Pekur <maxpekur88@gmail.com>
2023-01-31 12:11:32 +0100

    add post insta

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -12,7 +12,45 @@ import MailIcon from "../../components/ui/MailIcon";
 import PhoneIcon from "../../components/ui/PhoneIcon";
 import PointIcon from "../../components/ui/PointIcon";
 
-import insta from "../../public/assets/images/insta.png";
+import post1 from "../../public/assets/images/1.png";
+import post2 from "../../public/assets/images/2.png";
+import post3 from "../../public/assets/images/3.png";
+import post4 from "../../public/assets/images/4.png";
+import post5 from "../../public/assets/images/5.png";
+import post6 from "../../public/assets/images/6.png";
+
+const insta = [
+  {
+    id: "01",
+    img: post1,
+    href: "https://www.instagram.com/p/CoC42xlsGjm/?igshid=YmMyMTA2M2Y=",
+  },
+  {
+    id: "02",
+    img: post2,
+    href: "https://www.instagram.com/p/Cn17R-vsebO/?igshid=YmMyMTA2M2Y=",
+  },
+  {
+    id: "03",
+    img: post3,
+    href: "https://www.instagram.com/reel/Cnwwmi6Bwwb/?igshid=YmMyMTA2M2Y=",
+  },
+  {
+    id: "04",
+    img: post4,
+    href: "https://www.instagram.com/p/CnmdMLfMBUg/?igshid=YmMyMTA2M2Y=",
+  },
+  {
+    id: "05",
+    img: post5,
+    href: "https://www.instagram.com/p/CnhZyH6Mw8f/?igshid=YmMyMTA2M2Y=",
+  },
+  {
+    id: "06",
+    img: post6,
+    href: "https://www.instagram.com/reel/CnXHQgwh68s/?igshid=YmMyMTA2M2Y=",
+  },
+];
 
 export async function getStaticProps({ locale }) {
   return {
@@ -88,8 +126,12 @@ const index = ({ props }) => {
               fill={"#a2a2a2"}
             />
           </div>
-          <div className="flex items-center justify-center max-w-7xl m-auto lg:my-10 px-4">
-            <Image src={insta} alt="" />
+          <div className="max-w-7xl m-auto lg:my-10 px-4 grid grid-cols-2 md:grid-cols-3 gap-4">
+            {insta.map(({ id, href, img }) => (
+              <Link href={href} key={id}>
+                <Image src={img} width={500} height={500} />
+              </Link>
+            ))}
           </div>
         </section>
 

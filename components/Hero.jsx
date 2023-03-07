64d3fc17146d68dea1a2ad86e4a06d2ff5f9dd34
Maks Pekur <maxpekur88@gmail.com>
2023-03-07 17:26:54 +0100

    fixed

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,23 @@
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { useDevice } from '../hooks/useDevice'
 import figure from '../public/assets/images/fgs-hero.jpeg'
 import { hero } from '../translations/hero'
 import { Heading } from './ui/Heading'
 
 export const Hero = () => {
 	const { locale } = useRouter()
+	const device = useDevice()
+
+	const Elem = ({ children }) => {
+		const Tag = device === 'mobile' ? 'div' : 'motion.div'
+		return <Tag>{children}</Tag>
+	}
+
 	return (
 		<section id="hero" className="sticky top-0">
-			<div className="relative w-full h-screen">
+			<div className="relative w-full min-h-screen">
 				<motion.div
 					initial={{
 						scale: 1.5,
@@ -22,11 +30,18 @@ export const Hero = () => {
 					transition={{ duration: 1.5 }}
 					className="absolute inset-0"
 				>
-					<Image src={figure} fill />
+					<Image
+						src={figure}
+						fill
+						style={{
+							objectFit: 'cover',
+							objectPosition: 'center',
+						}}
+					/>
 				</motion.div>
 
-				<div className="absolute inset-0 text-white flex items-center md:items-end md:left-10 md:w-[55%]">
-					<motion.div
+				<div className="absolute inset-6 text-white flex items-center md:items-end md:left-10 md:w-[55%] mb-24">
+					<Elem
 						initial={{
 							y: '100%',
 							x: '-100%',
@@ -42,13 +57,14 @@ export const Hero = () => {
 							opacity: 1,
 						}}
 						transition={{ duration: 1.2 }}
-						className="space-y-6 mb-24 px-4"
 					>
-						<Heading tag={'h1'}>{hero.title[locale]}</Heading>
+						<Heading tag={'h1'} classNames={'mb-8'}>
+							{hero.title[locale]}
+						</Heading>
 						<Heading tag={'h4'} classNames="text-gray-400">
 							{hero.subtitle[locale]}
 						</Heading>
-					</motion.div>
+					</Elem>
 				</div>
 			</div>
 		</section>

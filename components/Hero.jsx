ddef2fba29878a19ce50c634b59dde152dedf10f
Maks Pekur <maxpekur88@gmail.com>
2023-03-07 17:42:34 +0100

    fixed

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,25 +9,19 @@ import { Heading } from './ui/Heading'
 export const Hero = () => {
 	const { locale } = useRouter()
 	const device = useDevice()
-
-	const Elem = ({ children }) => {
-		const Tag = device === 'mobile' ? 'div' : 'motion.div'
-		return <Tag>{children}</Tag>
-	}
-
 	return (
-		<section id="hero" className="sticky top-0">
+		<section id="hero" className="sticky top-0 overflow-hidden w-full h-full">
 			<div className="relative w-full min-h-screen">
 				<motion.div
 					initial={{
-						scale: 1.5,
+						scale: 1.2,
 						opacity: 0,
 					}}
 					animate={{
 						scale: 1,
 						opacity: 1,
 					}}
-					transition={{ duration: 1.5 }}
+					transition={{ duration: 1.7 }}
 					className="absolute inset-0"
 				>
 					<Image
@@ -41,22 +35,20 @@ export const Hero = () => {
 				</motion.div>
 
 				<div className="absolute inset-6 text-white flex items-center md:items-end md:left-10 md:w-[55%] mb-24">
-					<Elem
+					<motion.div
 						initial={{
 							y: '100%',
 							x: '-100%',
-							rotate: '50',
 							scale: 0,
 							opacity: 0,
 						}}
 						animate={{
 							y: 0,
 							x: 0,
-							rotate: 0,
 							scale: 1,
 							opacity: 1,
 						}}
-						transition={{ duration: 1.2 }}
+						transition={{ duration: 1 }}
 					>
 						<Heading tag={'h1'} classNames={'mb-8'}>
 							{hero.title[locale]}
@@ -64,7 +56,7 @@ export const Hero = () => {
 						<Heading tag={'h4'} classNames="text-gray-400">
 							{hero.subtitle[locale]}
 						</Heading>
-					</Elem>
+					</motion.div>
 				</div>
 			</div>
 		</section>

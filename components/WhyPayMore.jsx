79ef4eae52d06b525ecaf2b218ffe129c853c607
Maks Pekur <maxpekur88@gmail.com>
2023-03-07 17:00:13 +0100

    fix

--- components/WhyPayMore.jsx
+++ components/WhyPayMore.jsx
@@ -3,7 +3,7 @@ import { Heading } from './ui/Heading'
 
 export const WhyPayMore = () => {
 	return (
-		<section className="bg-[var(--dark-gray)] rounded-3xl px-20 py-10 sticky top-0 h-[150vh] ">
+		<section className="bg-[var(--dark-gray)] rounded-3xl px-10 md:px-20 md:py-10 sticky top-0 h-[150vh] ">
 			<div className="text-white">
 				<div className=" text-center pt-14 pb-20">
 					<Heading tag={'h3'}>
@@ -13,17 +13,17 @@ export const WhyPayMore = () => {
 
 				<div className="flex flex-col md:flex-row justify-between items-end gap-10 text-4xl mb-10">
 					<div className="w-full h-[250px] md:h-[450px] border-[1px] border-white rounded-3xl relative overflow-hidden">
-						<span className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-white font-bold text-2xl md:text-5xl whitespace-nowrap">
+						<span className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-white font-bold text-4xl md:text-5xl whitespace-nowrap">
 							~3653 PLN
 						</span>
 						<motion.div
 							initial={{ y: '100%' }}
 							whileInView={{ y: 0 }}
 							transition={{ duration: 1.5 }}
-							className="w-full h-full bg-gray-300 opacity-10 flex items-center justify-center"
+							className="w-full h-full bg-red-400 opacity-30 flex items-center justify-center"
 						></motion.div>
 					</div>
-					<div className="w-full border-[1px] bg-[var(--green)] text-black border-white rounded-3xl flex items-center justify-center p-20 whitespace-nowrap">
+					<div className="w-full border-[1px] bg-[var(--green)] text-black border-white rounded-3xl flex items-center justify-center p-8 md:p-20 whitespace-nowrap">
 						500 PLN
 					</div>
 				</div>

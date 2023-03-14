d0ca21e1e0b5a5c1887c449f6e7d81e5e336d0d0
Maks Pekur <maxpekur88@gmail.com>
2023-03-14 18:02:43 +0100

    fixed

--- pages/_app.js
+++ pages/_app.js
@@ -27,7 +27,7 @@ const App = ({ Component, pageProps }) => {
 			<CookieConsent
 				location="top"
 				buttonText="Ok"
-				cookieName="fbacoockie"
+				cookieName="fba_coockie"
 				style={{
 					background: '#2B373B',
 					display: 'flex',

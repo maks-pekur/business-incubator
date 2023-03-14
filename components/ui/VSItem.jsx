bb9844224adb59d1985c45854960a4ebc9632b82
Maks Pekur <maxpekur88@gmail.com>
2023-03-14 13:40:26 +0100

    fix

--- components/ui/VSItem.jsx
+++ components/ui/VSItem.jsx
@@ -9,7 +9,7 @@ export const VSItem = ({ variant, text, width, el }) => {
 					initial={{ x: '-100%' }}
 					whileInView={{ x: 0 }}
 					transition={{ duration: 1.2, ease: 'easeOut' }}
-					className={`bg-[var(--light-gray)] leading-1 rounded-r-full flex items-center justify-end h-14`}
+					className={`bg-[var(--light-gray)] leading-[10px] md:leading-5 rounded-r-full flex items-center justify-end h-14`}
 					style={{ width: width }}
 				>
 					<div className="mx-1 md:mx-3 sm:mx-6 whitespace-nowrap text-center">
@@ -22,7 +22,7 @@ export const VSItem = ({ variant, text, width, el }) => {
 					initial={{ x: '100%' }}
 					whileInView={{ x: 0 }}
 					transition={{ duration: 1.2, ease: 'easeOut' }}
-					className={`bg-white leading-1 rounded-l-full w-full flex-row-reverse flex items-center justify-end h-14`}
+					className={`bg-white leading-[10px] md:leading-5 rounded-l-full w-full flex-row-reverse flex items-center justify-end h-14`}
 				>
 					<div className="mx-1 sm:mx-6 md:whitespace-nowrap text-center">
 						{text}
